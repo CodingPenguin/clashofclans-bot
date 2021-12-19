0f6701cdd664107986f38ffa406539d7ac3a9d53
CodingPenguin <nguyenhuydanny@gmail.com>
2021-12-19 03:01:31 -0600

    Stupid env (major bug fix v9)

--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
-API_KEY = os.environ["API_KEY"]
-TOKEN = os.environ["TOKEN"]
+API_KEY = os.environ.get("API_KEY")
+TOKEN = os.environ.get("TOKEN")
 PROXIES = {
-    "http": os.environ['QUOTAGUARD_STATIC_URL'],
-    "https": os.environ['QUOTAGUARD_STATIC_URL']
+    "http": os.environ.get('QUOTAGUARD_STATIC_URL'),
+    "https": os.environ.get('QUOTAGUARD_STATIC_URL')
 }   
-MONGO_SRV_URL = os.environ['MONGO_SRV_URL']
+MONGO_SRV_URL = os.environ.get('MONGO_SRV_URL')
